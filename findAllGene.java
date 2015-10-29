d17c7473d7165a654046e7b73c1c202d81a01fc4
ylchang <yuli2.chang@gmail.com>
2015-10-28 23:02:08 -0500

    modify findAllGene.java. working

--- findAllGene.java
+++ findAllGene.java
@@ -7,42 +7,49 @@ import edu.duke.*;
  */
 public class findAllGene {
     public int findStopIndex(String dna, int index){
-        System.out.println(dna+ " " + index);
+       // System.out.println(dna+ " " + index);
         int stop1 = dna.indexOf("tag", index);
-        System.out.println("stop1 "+ stop1);
+       //System.out.println("stop1 "+ stop1);
         if(stop1 == -1 || (stop1-index)%3 !=0){
             stop1 = dna.length();
         }
         int stop2 = dna.indexOf("tga", index);
-        System.out.println("stop2 "+ stop2);
+       // System.out.println("stop2 "+ stop2);
         if(stop2 == -1 || (stop2-index)%3 !=0){
             stop2 = dna.length();
         }
         int stop3 = dna.indexOf("taa", index);
-        System.out.println("stop3 "+ stop3);
+       // System.out.println("stop3 "+ stop3);
         if(stop3 == -1 || (stop3-index)%3 !=0){
             stop3 = dna.length();
         }
-        System.out.println(stop1 + " " + stop2 + " " + stop3);
+       //System.out.println(stop1 + " " + stop2 + " " + stop3);
         return Math.min(stop1,Math.min(stop2,stop3));
     }
     public void printAll(String dna){
-        System.out.println("DNA string is: " + dna);
+        //System.out.println("DNA string is: " + dna);
         String dnaLower = dna.toLowerCase();
         int beg = 0;
         while(true){
-            int loc = dnaLower.indexOf("atg",beg);
-            if(loc == -1){
+            int log = dnaLower.indexOf("atg",beg);
+            if(log == -1){
             break;
             }
-            int stop = findStopIndex(dnaLower, beg);
-            if(beg > dnaLower.length()-3 && stop == dnaLower.length()){
-                break;
+            //System.out.println("LOG " + log);
+            int stop = findStopIndex(dnaLower, log);
+            if(stop == dnaLower.length()){
+                if(beg > dnaLower.length()-6){
+                    break;
+                }
+                else {
+                    beg = log + 3;    
+                }
             } 
-            if(beg < dnaLower.length()-3 && stop < dnaLower.length()){
-                System.out.println("Gene found: " + dna.substring(beg, stop+3));
+            else {
+                System.out.println(dna.substring(log,stop+3));
+                //System.out.println("HERE " + beg + " " + stop);
+                beg = stop + 3;
             }
-            beg = loc + 3;
         }
     }
     public void testFinder(){
